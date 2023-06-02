e89a57c90f8453ffd728e266c22d3c793b990444
TTolsun <baboess@gmail.com>
2023-06-02 13:09:18 +0900

    [feature] same char
    Signed-off-by: TTolsun <baboess@gmail.com>

--- SimilarityCheckerSource/SimilarityChecker.cpp
+++ SimilarityCheckerSource/SimilarityChecker.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <set>
 
 using namespace std;
 
@@ -9,10 +10,34 @@ public:
 	{
 		string longStr{}, shortStr{};
 		compareStringLength(str1, str2, longStr, shortStr);
-		return calculateScore(longStr, shortStr);
+		return calculateLengthScore(longStr, shortStr);
+	}
+
+	static int checkSameAlpha(const std::string& str1, const std::string& str2)
+	{
+		return calculateSameAlphaScore({ str1, str2 });
 	}
 
 private:
+	struct CharSet
+	{
+		std::set<char> strSet{}, str1Set{}, str2Set{};
+
+		CharSet(const std::string& str1, const std::string& str2)
+		{
+			for (const auto& ch : str1)
+			{
+				strSet.insert(ch);
+				str1Set.insert(ch);
+			}
+			for (const auto& ch : str2)
+			{
+				strSet.insert(ch);
+				str2Set.insert(ch);
+			}
+		}
+	};
+
 	static void compareStringLength(const string& str1, const string& str2, string& longStr,
 	                                string& shortStr)
 	{
@@ -28,10 +53,22 @@ private:
 		}
 	}
 
-	static int calculateScore(const string& longStr, const string& shortStr)
+	static int calculateLengthScore(const string& longStr, const string& shortStr)
 	{
 		const auto gapValue = longStr.length() - shortStr.length();
 		const auto divideValue = static_cast<float>(gapValue) / static_cast<float>(shortStr.length());
 		return static_cast<int>((1.0f - divideValue) * 60.0f);
+	}
+
+	static int calculateSameAlphaScore(const CharSet& charSet)
+	{
+		const auto totalCnt = charSet.strSet.size();
+		int sameCnt{};
+		for (const auto& ch : charSet.str1Set)
+		{
+			if (charSet.str2Set.count(ch))
+				sameCnt++;
+		}
+		return static_cast<int>((static_cast<float>(sameCnt) / static_cast<float>(totalCnt)) * 40.f);
 	}
 };
--- SimilarityCheckerTest/SimilarityCheckerTest.cpp
+++ SimilarityCheckerTest/SimilarityCheckerTest.cpp
@@ -7,3 +7,11 @@ TEST(LengthCheck, LengthCheck) {
 	EXPECT_EQ(SimilarityChecker::checkLength("AAABB", "BAA"), 19);
 	EXPECT_EQ(SimilarityChecker::checkLength("AA", "AAA"), 30);
 }
+
+TEST(LengthCheck, SameAlphaCheck)
+{
+	EXPECT_EQ(SimilarityChecker::checkSameAlpha("ASD", "DSA"), 40);
+	EXPECT_EQ(SimilarityChecker::checkSameAlpha("A", "BB"), 0);
+	EXPECT_EQ(SimilarityChecker::checkSameAlpha("AAABB", "BAA"), 40);
+	EXPECT_EQ(SimilarityChecker::checkSameAlpha("AA", "AAE"), 20);
+}
